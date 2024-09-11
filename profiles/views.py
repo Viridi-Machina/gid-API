c0b2a9956d85105486a177e8ab80c1886f25e362
jacen green <jacen.l.green@hotmail.com>
2024-09-11 11:30:37 +0100

    remove unused temporary import from profile view for push

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
+# from django_filters.rest_framework import DjangoFilterBackend
 
 from gid_API.permissions import IsOwnerOrReadOnly
 from .models import Profile
